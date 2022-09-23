7af7a70b51d102a3563c9f74e9d5150c989943ce
JINSOUL KIM <wlsthf75@gamil.com>
2022-09-24 00:19:01 +0100

    Add clock

--- js/greetings.js
+++ js/greetings.js
@@ -1,11 +1,11 @@
-const loginForm = document.querySelector("#login-form");
+const loginForm = document.getElementById("login-form")
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const greeting = document.getElementById("greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
+function onLoginFromSubmit(event){
     event.preventDefault();
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
@@ -13,15 +13,16 @@ function onLoginSubmit(event){
     paintGreeting(username);
 }
 
-function paintGreeting(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);}
+function paintGreeting(savedUsername){
+    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null) {
+if (savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+    loginForm.addEventListener("submit", onLoginFromSubmit);
 } else {
     paintGreeting(savedUsername);
 }
